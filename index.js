0e56e4d35b421a2cc87cba6a14386811eab5781d
jesse keane <me@jessekeane.me>
2013-06-23 04:55:08 +0400

    some simple stuffs

--- index.js
+++ index.js
@@ -3,20 +3,47 @@ var util = require('util'),
     irc = require('irc');
 
 function Ziggy(settings) {
-  this.client = new irc.Client
+
+  this.settings = settings;
+  this.client = new irc.Client(settings.server, settings.nickname,
+                               { channels: settings.channels,
+                                 userName: 'ziggy',
+                                 realName: 'Ziggy',
+                                 secure: settings.secure });
+
+  this.client.addListener();
 }
 
-Ziggy.prototype
+Ziggy.prototype.part = function (channels) {
+  if (channels instanceof Array) {
+    channels.forEach(channel) { this.part(channel); }.bind(this));
+    return;
+  }
+
+  this.client.part(channels);
+};
+
+Ziggy.prototype.join = function (channels) {
+  if (channels instanceof Array) {
+    channels.forEach(channel) { this.join(channel); }.bind(this));
+    return;
+  }
+
+  this.client.join(channels);
+};
+
+Ziggy.prototype.say = function (target, 
 
 function start(options) {
   var settings = {};
 
   settings.server = options.server || 'irc.freenode.org';
-  settings.channel = options.channel || '#test';
+  settings.channels = options.channels || [];
   settings.nickname = options.nickname || 'Ziggy';
   settings.plugins = options.plugins || [];
   settings.password = options.password || '';
   settings.groups = options.groups || {};
+  settings.secure = options.secure;
 
   return new Ziggy(settings);
 }
