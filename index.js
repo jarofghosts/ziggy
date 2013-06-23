1b3f0602d2464588abb029b312ee1f77a7e4c9fb
jesse keane <me@jessekeane.me>
2013-06-23 04:25:11 +0400

    getting basic architecture down

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ var util = require('util'),
     EventEmitter = require('events').EventEmitter,
     irc = require('irc');
 
-function Ziggy() {
+function Ziggy(settings) {
+  this.client = new irc.Client
 }
 
-function makeZiggy() {
+Ziggy.prototype
+
+function start(options) {
+  var settings = {};
+
+  settings.server = options.server || 'irc.freenode.org';
+  settings.channel = options.channel || '#test';
+  settings.nickname = options.nickname || 'Ziggy';
+  settings.plugins = options.plugins || [];
+  settings.password = options.password || '';
+  settings.groups = options.groups || {};
+
+  return new Ziggy(settings);
 }
 
-module.exports = makeZiggy;
+util.inherits(Ziggy, EventEmitter);
+
+module.exports = start;
