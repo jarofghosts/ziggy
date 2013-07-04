1507dc9e4a9adf4e1ac6638b90e3f12986b24d5a
jesse keane <me@jessekeane.me>
2013-07-04 19:51:15 +0400

    moved lib out

--- index.js
+++ index.js
@@ -3,68 +3,6 @@ var util = require('util'),
     irc = require('irc'),
     extend = require('xtend'),
     noop = function () {};
-
-function activatePlugins(ziggy) {
-  var i = 0,
-      l = ziggy.settings.plugins.length;
-  for (; i < l; ++i) {
-    ziggy.settings.plugins[i](ziggy);
-  }
-}
-
-function populateUsers(ziggy, myUsers) {
-
-  if (!ziggy.settings.users) { ziggy.settings.users = {}; }
-
-  var users = Object.keys(myUsers),
-      i = 0,
-      l = users.length;
-
-  for (; i < l; ++i) {
-    var thisUser = users[i];
-    ziggy.settings.users[thisUser] = myUsers[thisUser];
-    ziggy.settings.users[thisUser].shared = {
-      userLevel: ziggy.settings.users[thisUser].userLevel,
-      authenticated: ziggy.settings.users[thisUser].password === undefined,
-      whois: null
-    };
-  }
-}
-
-function massExecute(ziggy, command, items, callback) {
-
-  callback = callback || noop;
-
-  var i = 0,
-      l = items.length;
-
-  for (; i < l; ++i) {
-    if (i !== l - 1) { callback = noop; }
-    this[command](items[i], callback);
-  }
-
-}
-
-function userList(ziggy, callback) {
-  var users = Object.keys(this.settings.users),
-      i = 0,
-      l = users.length,
-      userList = {};
-
-  for (; i < l; ++i) {
-    userList[users[i]] = this.settings.users[users[i]].shared;
-    if (i === l) { callback && callback(userList); }
-  }
-}
-
-function lookupUser(ziggy, nickname) {
-  var userInfo = ziggy.settings.users[nickname] ? ziggy.settings.users[nickname].shared : null;
-  return {
-    nick: nickname,
-    info: userInfo
-  };
-}
-
 function Ziggy(settings) {
 
   this.settings = {};
